4710db063fc499963e3da3e263168fe2e8588c80
Sonam Lama <sonamtlama@gmail.com>
2019-01-10 23:45:53 -0800

    homework files updated

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,7 +1,7 @@
-// var random = Math.floor(Math.random() * 9) + 1;
 $(document).ready(function() 
 {
-  function getRndInteger(min, max) 
+  //creating a function to get random number between min & max
+    function getRndInteger(min, max) 
   {
     return Math.floor(Math.random() * (max - min + 1) ) + min;
   };
@@ -13,88 +13,85 @@ $(document).ready(function()
   var wins = 0;
   var losses = 0;
 
-  // Now for the hard part. Creating multiple crystals each with their own unique number value.
-
-  // We begin by expanding our array to include four options.
-  var numberOptions = [getRndInteger(1,12), getRndInteger(1,12), getRndInteger(1,12), getRndInteger(1,12)];
-
-  // Next we create a for loop to create crystals for every numberOption.
-  for (var i = 0; i < numberOptions.length; i++) {
-
-    // For each iteration, we will create an imageCrystal
-    var imageCrystal = $("<img>");
-    var imgNum = i;
-
-    // First each crystal will be given the class ".crystal-image".
-    // This will allow the CSS to take effect.
-    imageCrystal.addClass("crystal-image");
-
-    // Each imageCrystal will be given a src link to the crystal image
-    if (imgNum===0)
-        {
-        imageCrystal.attr(src="assets/images/blue.jpg");
-        }
-    else if(imgNum===1)
-        {
-        imageCrystal.attr(src="assets/images/orange.jpg");
-        }
-    else if(imgNum===2)
-        {
-        imageCrystal.attr(src="assets/images/purple.jpg");
-        }
-    else if(imgNum===3)
-        {
-        imageCrystal.attr(src="assets/images/red.jpg");
-        }
-    // Each imageCrystal will be given a data attribute called data-crystalValue.
-    // This data attribute will be set equal to the array value.
-    imageCrystal.attr("data-crystalvalue", numberOptions[i]);
-
-    // Lastly, each crystal image (with all it classes and attributes) will get added to the page.
-    $("#crystals").append(imageCrystal);
+  var firstNum = getRndInteger(1,12);
+  var secondNum = getRndInteger(1,12);
+  var thirdNum = getRndInteger(1,12);
+  var fourthNum = getRndInteger(1,12);
+  //creating a function that resets the game and the desired values
+  function reset()
+  {
+    counter = 0;
+    $("#your-score").text(counter);
+    targetNumber = getRndInteger(19,120);
+    $("#number-to-guess").text(targetNumber);
+    firstNum = getRndInteger(1,12);
+    secondNum = getRndInteger(1,12);
+    thirdNum = getRndInteger(1,12);
+    fourthNum = getRndInteger(1,12);
   }
 
-  // This time, our click event applies to every single crystal on the page. Not just one.
-  $(".crystal-image").on("click", function() {
+  function winner()
+  {
+    wins++;        
+    $("#wins-losses").html("You scored: " + counter + " You win!  Click Crystals to play again!"+ "<br>" + "Wins: " + wins + "<br>"+"Losses: " + losses);
+    reset();
+  }
 
-    // Determining the crystal's value requires us to extract the value from the data attribute.
-    // Using the $(this) keyword specifies that we should be extracting the crystal value of the clicked crystal.
-    // Using the .attr("data-crystalvalue") allows us to grab the value out of the "data-crystalvalue" attribute.
-    // Since attributes on HTML elements are strings, we must convert it to an integer before adding to the counter
-
-    var crystalValue = ($(this).attr("data-crystalvalue"));
-    crystalValue = parseInt(crystalValue);
-    // We then add the crystalValue to the user's "counter" which is a global variable.
-    // Every click, from every crystal adds to the global counter.
-    counter += crystalValue;
-
-    // All of the same game win-lose logic applies. So the rest remains unchanged.
-    // alert("New score: " + counter);
+  function loser()
+  {
+    losses++;        
+    $("#wins-losses").html("You scored: " + counter + " You lose!  Click Crystals to play again!"+ "<br>" + "Wins: " + wins + "<br>"+"Losses: " + losses);
+    reset();
+  }
+  //
+  $('#first').on('click', function(){
+    counter += firstNum;
     $("#your-score").text(counter);
     $("#wins-losses").html("Wins: " + wins + "<br>"+ "Losses: " + losses);
-
-    if (counter === targetNumber) {
-        wins++;        
-        $("#wins-losses").html("You scored: " + counter + " You win!  Click Crystals to play again!"+ "<br>" + "Wins: " + wins + "<br>"+"Losses: " + losses);
-        counter = 0;
+          //sets win/lose conditions
+        if (counter === targetNumber){
+          winner();
+        }
+        else if ( counter > targetNumber){
+          loser();
+        }  
+    }) 
+    $('#second').on ('click', function(){
+        counter += secondNum;
         $("#your-score").text(counter);
-        crystalValue = 0;
-        numberOptions = [getRndInteger(1,12), getRndInteger(1,12), getRndInteger(1,12), getRndInteger(1,12)];
-        targetNumber = getRndInteger(19,120);
-        $("#number-to-guess").text(targetNumber);
-    }
+        $("#wins-losses").html("Wins: " + wins + "<br>"+ "Losses: " + losses);
+              //sets win/lose conditions
+            if (counter === targetNumber){
+              winner();
+            }
+            else if ( counter > targetNumber){
+              loser();
+            }  
+        }) 
+        $('#third').on ('click', function(){
+            counter += thirdNum;
+            $("#your-score").text(counter);
+            $("#wins-losses").html("Wins: " + wins + "<br>"+ "Losses: " + losses);
+                  //sets win/lose conditions
+                if (counter === targetNumber){
+                  winner();
+                }
+                else if ( counter > targetNumber){
+                  loser();
+                }  
+            }) 
+            $('#fourth').on ('click', function(){
+                counter += fourthNum;
+                $("#your-score").text(counter);
+                $("#wins-losses").html("Wins: " + wins + "<br>"+ "Losses: " + losses);
+                      //sets win/lose conditions
+                    if (counter === targetNumber){
+                      winner();
+                    }
+                    else if ( counter > targetNumber){
+                      loser();
+                    }  
+                }); 
+            }); 
 
-    else if (counter > targetNumber) {
-        losses++;        
-        $("#wins-losses").html("You scored: " + counter + " You lose!  Click Crystals to play again!"+ "<br>" + "Wins: " + wins + "<br>"+"Losses: " + losses);
-        counter = 0;
-        $("#your-score").text(counter);
-        crystalValue = 0;
-        numberOptions = [getRndInteger(1,12), getRndInteger(1,12), getRndInteger(1,12), getRndInteger(1,12)];
-        targetNumber = getRndInteger(19,120);
-        $("#number-to-guess").text(targetNumber);
-    }
-
-  });
-});
 
